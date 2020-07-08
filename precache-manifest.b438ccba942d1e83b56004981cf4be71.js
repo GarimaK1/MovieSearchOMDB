06fda5f2dd79e0f3fa68910fb26f90a196c1f925
Garima <garima.k.03@gmail.com>
2020-07-08 12:31:53 -0400

    Updates

---
+++ precache-manifest.b438ccba942d1e83b56004981cf4be71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051471a28f661feaac1c2a2a4b35e54b",
+    "url": "/MovieSearchOMDB/index.html"
+  },
+  {
+    "revision": "c398b57ed76ee66ae63f",
+    "url": "/MovieSearchOMDB/static/css/main.e0d02f67.chunk.css"
+  },
+  {
+    "revision": "70767b51cbf9b33f8f70",
+    "url": "/MovieSearchOMDB/static/js/2.7604d02a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MovieSearchOMDB/static/js/2.7604d02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c398b57ed76ee66ae63f",
+    "url": "/MovieSearchOMDB/static/js/main.abc77535.chunk.js"
+  },
+  {
+    "revision": "7a65cd28a61f376426e8",
+    "url": "/MovieSearchOMDB/static/js/runtime-main.be76d972.js"
+  }
+]);

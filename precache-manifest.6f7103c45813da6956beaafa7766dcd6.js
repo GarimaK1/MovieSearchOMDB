c9bb4fef966d3ad213f9b2b3b7ca9de6683fcc8f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-08 23:09:54 +0000

    Deploy GarimaK1/MovieSearchOMDB to github.com/GarimaK1/MovieSearchOMDB.git:gh-pages

--- precache-manifest.6f7103c45813da6956beaafa7766dcd6.js
+++ precache-manifest.6f7103c45813da6956beaafa7766dcd6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5ac72466899d29e2aa7d227ed187abc",
+    "revision": "7fd2dfefaef3a1c2e3546b5d909547b4",
     "url": "/MovieSearchOMDB/index.html"
   },
   {
-    "revision": "ab0e74e2b2030eaeae35",
+    "revision": "ed285e1d9378416e593c",
     "url": "/MovieSearchOMDB/static/css/main.e0d02f67.chunk.css"
   },
   {
-    "revision": "73856aa6b756dae746f2",
-    "url": "/MovieSearchOMDB/static/js/2.a6cc4956.chunk.js"
+    "revision": "de4b2f7da6792c451a9b",
+    "url": "/MovieSearchOMDB/static/js/2.2466b8fe.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/MovieSearchOMDB/static/js/2.a6cc4956.chunk.js.LICENSE.txt"
+    "url": "/MovieSearchOMDB/static/js/2.2466b8fe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ab0e74e2b2030eaeae35",
-    "url": "/MovieSearchOMDB/static/js/main.f27e5983.chunk.js"
+    "revision": "ed285e1d9378416e593c",
+    "url": "/MovieSearchOMDB/static/js/main.aa979faf.chunk.js"
   },
   {
     "revision": "7a65cd28a61f376426e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MovieSearchOMDB/precache-manifest.3148d11cc687c8c8ff3c7b9e140a173c.js"
+  "/MovieSearchOMDB/precache-manifest.6f7103c45813da6956beaafa7766dcd6.js"
 );
 
 self.addEventListener('message', (event) => {
